27d09abd7fca104bf46381db851a88fe8898f733
Marialostudio <89900388+Marialostudio@users.noreply.github.com>
2024-05-14 11:08:38 -0500

    nueva api

---
+++ angular-car/src/app/services/data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(/*private http:HttpClient*/) { }
+
+  /*obtenerDatos () {
+    const url = 'https://api.api-ninjas.com/v1/car';
+    const key = 'e+3P473CVoeAtZt1QAbVcA==7e49cDIda0wz2PCr';
+    const header = new HttpHeaders({'x-api-key': key});
+    return this.http.get(url, {headers: header});
+  }*/
+
+  API_URL: string = "https://thronesapi.com/swagger/v2/swagger.json";
+
+  httpClient = inject (HttpClient);
+
+  obtenerDatos() {
+    return this.httpClient.get(this.API_URL);
+  }
+
+}
